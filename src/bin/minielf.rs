ef70543691e178a67495031989788a6e3de8a908
Sebastian Morr <sebastian@morr.cc>
2019-03-30 00:40:04 +0100

    Build 64-bit ELF binary

--- src/bin/minielf.rs
+++ src/bin/minielf.rs
@@ -6,23 +6,23 @@ use std::io::prelude::*;
 
 struct Segment {
     typ: u32,
-    address: u32,
-    size: u32,
+    address: u64,
+    size: u64,
     flags: u32,
     code: Vec<u8>,
 }
 
 fn main() -> std::io::Result<()> {
-    let start_address = 0x8000000;
-    let header_size = 4 + 4 + 8 + 8 * 2 + 5 * 4;
-    let pht_entry_size = 8 * 4;
+    let start_address = 0x6000000;
+    let header_size = 4 + 4 + 8 + 8 * 2 + 2 * 4 + 3 * 8;
+    let pht_entry_size = 2 * 4 + 6 * 8;
 
-    let quit = vec![0xbb, 0x2a, 0, 0, 0, 0xb8, 1, 0, 0, 0, 0xcd, 0x80];
+    let quit = vec![0xb8, 0x3c, 0, 0, 0, 0xbf, 0x2a, 0, 0, 0, 0xf, 0x5];
 
     let main_segment = Segment {
         typ: 1,
         address: start_address,
-        size: 5,
+        size: header_size + pht_entry_size + quit.len() as u64,
         flags: 5,
         code: quit,
     };
@@ -35,7 +35,7 @@ fn main() -> std::io::Result<()> {
     buffer.write(&[0x7f, 'E' as u8, 'L' as u8, 'F' as u8])?;
 
     // 32-bit format (1) or 64-bit format (2).
-    buffer.write(&[1])?;
+    buffer.write(&[2])?;
 
     // Little endian (1) or big endian (2).
     buffer.write(&[1])?;
@@ -55,21 +55,21 @@ fn main() -> std::io::Result<()> {
     buffer.write_u16::<LittleEndian>(2)?;
 
     // Instruction set architecture. x86 is 3.
-    buffer.write_u16::<LittleEndian>(3)?;
+    buffer.write_u16::<LittleEndian>(62)?;
 
     // Always set to 1?
     buffer.write_u32::<LittleEndian>(1)?;
 
     // Address of the entry point.
-    buffer.write_u32::<LittleEndian>(
-        start_address + header_size + pht_entry_size * (segments.len() as u32),
+    buffer.write_u64::<LittleEndian>(
+        start_address + header_size + pht_entry_size * (segments.len() as u64),
     )?;
 
     // Start of the program header table.
-    buffer.write_u32::<LittleEndian>(header_size)?;
+    buffer.write_u64::<LittleEndian>(header_size)?;
 
     // Start of the section header table.
-    buffer.write_u32::<LittleEndian>(0)?;
+    buffer.write_u64::<LittleEndian>(0)?;
 
     // "flags"
     buffer.write_u32::<LittleEndian>(0)?;
@@ -98,26 +98,26 @@ fn main() -> std::io::Result<()> {
         // Type of the segment. Loadable segment is 1.
         buffer.write_u32::<LittleEndian>(segment.typ)?;
 
-        // Offset.
-        buffer.write(&[0; 4])?;
-
-        // Virtual address of the segment in memory.
-        buffer.write_u32::<LittleEndian>(segment.address)?;
-
-        // Physical address of the segment in memory.
-        buffer.write_u32::<LittleEndian>(segment.address)?;
-
-        // Size of the segment in the file image.
-        buffer.write_u32::<LittleEndian>(segment.size)?;
-
-        // Size of the segment in memory.
-        buffer.write_u32::<LittleEndian>(segment.size)?;
-
         // Segment-dependent flags.
         buffer.write_u32::<LittleEndian>(segment.flags)?;
 
+        // Offset.
+        buffer.write_u64::<LittleEndian>(0)?;
+
+        // Virtual address of the segment in memory.
+        buffer.write_u64::<LittleEndian>(segment.address)?;
+
+        // Physical address of the segment in memory.
+        buffer.write_u64::<LittleEndian>(segment.address)?;
+
+        // Size of the segment in the file image.
+        buffer.write_u64::<LittleEndian>(segment.size)?;
+
+        // Size of the segment in memory.
+        buffer.write_u64::<LittleEndian>(segment.size)?;
+
         // Alignment.
-        buffer.write_u32::<LittleEndian>(0)?;
+        buffer.write_u64::<LittleEndian>(0)?;
     }
 
     // Code.
